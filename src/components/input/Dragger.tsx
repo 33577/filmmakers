b978317f8c718ad343be8e6332c097708c24eb4b
33577 <ukj7546@gmail.com>
2020-06-20 19:14:02 +0900

    Add dragger customRequest and iconRende

--- src/components/input/Dragger.tsx
+++ src/components/input/Dragger.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Upload } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
+import { UploadChangeParam } from 'antd/lib/upload';
 
 export default function Dragger({handleSubmit}: {handleSubmit: (chatLog: string) => void}) {
   const props = {
     name: 'file',
     multiple: true,
-    // action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-    onChange(info: any) {
-      var reader = new FileReader();
-      reader.readAsText(info.file.originFileObj);
-      reader.onload = function () {
-        handleSubmit(reader.result as string);
-      };
+    customRequest: () =>{},
+    iconRender: () => <> </>,
+    onChange(info: UploadChangeParam) {
+      if (typeof info.file.originFileObj !== "undefined") { 
+        const reader = new FileReader();
+        reader.readAsText(info.file.originFileObj);
+        reader.onload = function () {
+          handleSubmit(reader.result as string);
+        };
+      }
     },
   };
   const { Dragger } = Upload;
@@ -22,11 +26,8 @@ export default function Dragger({handleSubmit}: {handleSubmit: (chatLog: string)
         <p className="ant-upload-drag-icon">
             <InboxOutlined />
         </p>
-        <p className="ant-upload-text">Click or drag file to this area to upload</p>
-        <p className="ant-upload-hint">
-            Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-            band files
-        </p>
+        <p className="ant-upload-text">클릭하거나 파일을 끌어다 놓으세요</p>
+        <p className="ant-upload-hint">.log 형식</p>
     </Dragger>
   )
 }
