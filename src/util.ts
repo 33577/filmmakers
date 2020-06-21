7a0ebd548d26a36126bea78dd574b65e45c991d2
33577 <ukj7546@gmail.com>
2020-06-21 22:35:17 +0900

    Refac: 함수 쪼개기 및 위치 변경

--- src/util.ts
+++ src/util.ts
@@ -12,51 +12,14 @@ type ParsedChat = {
     text: string; 
 }
 
-export type FilteringKeyword = "ALL" | "YOUTUBE" | "LAUGH" | "SURPRISE" | "QUESTION_MARK" |  "EXCLAMATION_MARK";
-const WORDS_LIKE_YOUTUBE = ["유튜브", "유튭", "유투브", "유투", "유튜", "유하"]
-
-export const DATE_FORMAT = "yyyy-MM-dd HH:mm:ss"
-
-const round = (x: number, unit: number) => {
-    return Math.floor(x / unit) * unit;
-}
-
-const validChat = (maybeChat: string) => {
-    return maybeChat[0] === "[" && maybeChat.includes("<")
-}
-
-const includes = (str: string, targets: string[]) => targets.some(t => str.includes(t));
-
 export const getChattingSpeed = (chatLog: string, keyword: FilteringKeyword = "ALL", timeIntervalInMs: number = 10000): ChattingSpeed => {
     // Assert that chatLog가 시간에 대해 오름차순으로 정렬됨. 
     if (chatLog.length === 0) {
         return []
     }
-    return chatLog.split("\n").filter(validChat).map<ParsedChat>(e => {
-        const { time, text } = parseChat(e);
-        const roundedTime = round(time, timeIntervalInMs);
-        return  {
-            time: roundedTime,
-            text,
-        }
-    }).filter(({text}) => {
-        switch (keyword) {
-            case "ALL":
-                return true;
-            case "YOUTUBE":
-                return includes(text, WORDS_LIKE_YOUTUBE);
-            case "LAUGH":
-                return text.startsWith("ㅋㅋㅋ");
-            case "SURPRISE":
-                return text.startsWith("캬");
-            case "QUESTION_MARK":
-                return text.startsWith("?");
-            case "EXCLAMATION_MARK":
-                return text.includes("!");
-            default:
-                throw new Error("NOT REACHED");
-        }
-    }).reduce<ChattingSpeed>((acc, cur) => {
+    const parsedChatList = getParsedChatList(chatLog, timeIntervalInMs);
+
+    return parsedChatList.filter(({text}) => filterByKeyword(text, keyword)).reduce<ChattingSpeed>((acc, cur) => {
         const lastElement = last(acc);
         if (lastElement?.time === cur.time) {
             lastElement.frequency += 1;
@@ -69,6 +32,40 @@ export const getChattingSpeed = (chatLog: string, keyword: FilteringKeyword = "A
             texts: [cur.text]
         }]
     }, [])
+}
+
+const validChat = (maybeChat: string) => {
+  return maybeChat[0] === "[" && maybeChat.includes("<")
+}
+
+export const DATE_FORMAT = "yyyy-MM-dd HH:mm:ss"
+
+const floor = (x: number, unit: number) => {
+    return Math.floor(x / unit) * unit;
+}
+
+export type FilteringKeyword = "ALL" | "YOUTUBE" | "LAUGH" | "SURPRISE" | "QUESTION_MARK" |  "EXCLAMATION_MARK";
+const WORDS_LIKE_YOUTUBE = ["유튜브", "유튭", "유투브", "유투", "유튜", "유하"]
+
+const includes = (str: string, targets: string[]) => targets.some(t => str.includes(t));
+
+const filterByKeyword = (text: string, keyword: FilteringKeyword) => {
+  switch (keyword) {
+    case "ALL":
+        return true;
+    case "YOUTUBE":
+        return includes(text, WORDS_LIKE_YOUTUBE);
+    case "LAUGH":
+        return text.startsWith("ㅋㅋㅋ");
+    case "SURPRISE":
+        return text.startsWith("캬");
+    case "QUESTION_MARK":
+        return text.startsWith("?");
+    case "EXCLAMATION_MARK":
+        return text.includes("!");
+    default:
+        throw new Error("NOT REACHED");
+  }
 }
 
 export const parseChat = (chat: string): ParsedChat => {
@@ -86,4 +83,16 @@ export const parseChat = (chat: string): ParsedChat => {
     }
 }
 
-
+const getParsedChatList = (chatLog: string, timeIntervalInMs: number = 10000): ParsedChat[] => {
+  if (chatLog.length === 0) {
+    return []
+  }
+  return chatLog.split("\n").filter(validChat).map<ParsedChat>(e => {
+      const { time, text } = parseChat(e);
+      const flooredTime = floor(time, timeIntervalInMs);
+      return  {
+          time: flooredTime,
+          text,
+      }
+  })
+}
